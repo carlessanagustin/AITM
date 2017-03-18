9e479cadcda939e0be98fcf26a31d43fb2975b59
Carles San Agustin <info@carlessanagustin.com>
2017-03-18 08:18:59 +0100

    install_xescuder.sh on centos OK

--- 03_Jenkins/install_xescuder.sh
+++ 03_Jenkins/install_xescuder.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # provision environment
-yum -y update
-curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-yum -y install gcc-c++ make vim wget git
-yum -y install nodejs npm
+sudo yum -y update
+curl --silent --location https://rpm.nodesource.com/setup_4.x | sudo bash -
+sudo yum -y install gcc-c++ make vim wget git
+sudo yum -y install nodejs npm
 
 # deploy app
-cd /home/vagrant
+cd $HOME
 npm update
 git clone https://github.com/xescuder/ait.git
 cd ait
 npm install
 
 # run app
+cd $HOME/ait
 nohup bash ./scripts/web-server.sh &
