4af6b4bfc34883d408caa81f8375d11fd6deaac9
Carles San Agustin <info@carlessanagustin.com>
2017-04-04 06:12:09 +0200

    folder clean up + vagrantfile clean up

--- 03_Jenkins/app_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# deploy app
-cd $HOME
-npm update
-git clone https://github.com/xescuder/ait.git
-cd ait
-npm install
--- 03_Jenkins/app_run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# run app
-cd $HOME/ait
-nohup bash ./scripts/web-server.sh &
-exit 0
